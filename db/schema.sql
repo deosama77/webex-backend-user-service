21f3d5acb74c710362af22883e4e8ff5313293f3
deosama77 <deosama77@gmail.com>
2020-07-26 15:38:20 +0300

    updatd schema agin

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,20 @@
--- Table: public.projects
+-- Table: public.users
 
--- DROP TABLE public.projects;
+-- DROP TABLE public.users;
 
-CREATE TABLE public.projects
+CREATE TABLE public.users
 (
-    id integer NOT NULL DEFAULT nextval('products_id_seq'::regclass),
-    name text COLLATE pg_catalog."default",
-    status_progress text COLLATE pg_catalog."default",
-    status_provider text COLLATE pg_catalog."default",
-    complicity integer NOT NULL,
-    owner_id integer,
-    resources integer,
-    price integer,
-    provider text COLLATE pg_catalog."default",
-    start_date date,
-    end_date date,
-    offers numeric,
-    CONSTRAINT products_pkey PRIMARY KEY (id)
+    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+    first_name text COLLATE pg_catalog."default",
+    last_name text COLLATE pg_catalog."default",
+    email text COLLATE pg_catalog."default",
+    password text COLLATE pg_catalog."default",
+    date_birth date,
+    is_terms boolean,
+    CONSTRAINT users_pkey PRIMARY KEY (id)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE public.projects
+ALTER TABLE public.users
     OWNER to postgres;
